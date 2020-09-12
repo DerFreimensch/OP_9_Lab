eeb3897a2a7b5401d850828e314c7ba58b139e11
Der Freimensch <kostrm030102@gmail.com>
2020-09-12 19:51:01 +0300

    Добавьте файлы проекта.

---
+++ OP_9/Code.c
@@ -0,0 +1,35 @@
+//Скопировать содержимое текстового файла, ограничив длину строки N
+//символами.Слова, не помещающиеся в строку заданной длины, скопировать
+//в виде новой строки.
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define MAXLINE 1024
+FILE *fin;
+FILE *fout;
+int main(void) {
+	char line[MAXLINE];
+	char* ptr;
+	char c;
+	int N= 0;
+	int counter = 0;
+	fin = fopen("read.txt", "rt");
+	fout = fopen("write.txt", "wt");
+	if (fin == NULL || fout == NULL)
+		return;
+	scanf("%d", &N);
+	while (!feof(fin)) {
+		ptr = fgets(line, MAXLINE, fin);
+		c = *ptr;
+		if (ptr == NULL)
+			break;
+		while (counter++ != N) {
+			
+		}
+		*ptr++ = '\n';
+		ptr++;
+	}
+	fclose(fin);
+	fclose(fout);
+	return 0;
+
+}
