c3772a1540729d8d9e536eee25d805646f4bcd53
Der Freimensch <kostrm030102@gmail.com>
2020-10-08 21:54:35 +0300

    Last Version of the code

--- OP_9/Code.c
+++ OP_9/Code.c
@@ -1,35 +1,60 @@
-//Скопировать содержимое текстового файла, ограничив длину строки N
-//символами.Слова, не помещающиеся в строку заданной длины, скопировать
-//в виде новой строки.
+
 #define _CRT_SECURE_NO_WARNINGS
-#include <stdio.h>
 #define MAXLINE 1024
+#include <stdio.h>
+
+
 FILE *fin;
 FILE *fout;
-int main(void) {
-	char line[MAXLINE];
-	char* ptr;
-	char c;
-	int N= 0;
-	int counter = 0;
-	fin = fopen("read.txt", "rt");
-	fout = fopen("write.txt", "wt");
-	if (fin == NULL || fout == NULL)
-		return;
-	scanf("%d", &N);
+FILE* fout1;
+char line[MAXLINE];
+char* ptr;
+char* c;
+int N = 0;
+int counter = 0;
+void edit() {
 	while (!feof(fin)) {
 		ptr = fgets(line, MAXLINE, fin);
-		c = *ptr;
+		while (*ptr != '\0') {
+			if (*ptr == '\n') {
+				*ptr = ' ';
+			}
+			ptr++;
+		}
+		fputs(line, fout1);
+	}
+}
+int main(void) {
+	fin = fopen("read.txt", "rt");
+	fout = fopen("write.txt", "wt");
+	fout1 = fopen("rw.txt", "w+t");
+	if (fin == NULL || fout == NULL || fout1 == NULL)
+		return;
+	printf("%s\n", "Print N: ");
+	scanf_s("%d", &N);
+	edit();
+	fseek(fout1, 0, SEEK_SET);
+	while (!feof(fout1)) {
+		ptr = fgets(line, MAXLINE, fout1);
+		c = ptr;
 		if (ptr == NULL)
 			break;
-		while (counter++ != N) {
-			
+		while (*ptr != '\0') {
+			if (counter++ == N) {
+				ptr = c;
+				*ptr++ = '\n';
+				counter = 0;
+			}
+			if (*ptr == ' ') {
+				c = ptr;
+			}
+			ptr++;
 		}
-		*ptr++ = '\n';
-		ptr++;
+		fputs(line, fout);
 	}
 	fclose(fin);
 	fclose(fout);
+	fclose(fout1);
+	remove("rw.txt");
 	return 0;
-
 }
